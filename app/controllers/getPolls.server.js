88a70d2ded752578f61adc0ebf9665eca98a2014
demiacle <demiacle@gmail.com>
2017-12-11 15:50:13 -0800

    Added more info for all polls list

--- app/controllers/getPolls.server.js
+++ app/controllers/getPolls.server.js
@@ -1,12 +1,40 @@
 'use strict'
 var polls = require( '../models/polls.js')
+var users = require( '../models/users.js')
 var mongoose = require('mongoose')
 
 function getPolls(req, res, next){
-    //console.log(res)
-    polls.find({}, 'description owner pollOptions totalVotes', function(err, docs){
-        res.locals.polls = docs;
-        next();
+    polls.find({}, 'description owner pollOptions totalVotes', function(err, pollSchema){
+        if( res.locals.isLoggedIn ){
+            console.log(req.user)
+            var parsedPolls = pollSchema.map( function(i){
+                if( i.owner == req.user._id ){
+                    i.isCreator = true;
+                }
+                if( req.user.votesCast.includes( i._id ) == false ){
+                    i.hasVoted = true;
+                }
+                return i;
+            })
+            res.locals.polls = parsedPolls;
+            next();
+        } else {
+            var anonUser = users.findOneOrCreate({ ip : req.ip }, function(err, user) {
+                if(err)
+                    console.log(err)
+                var parsedPolls = pollSchema.map(function (i) {
+                    if (i.owner == user._id) {
+                        i.isCreator = true;
+                    }
+                    if (user.votesCast.includes(i._id) == false) {
+                        i.hasVoted = true;
+                    }
+                    return i;
+                })
+                res.locals.polls = parsedPolls;
+                next();
+            })
+        }
     });
 }
 
